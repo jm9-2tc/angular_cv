af4b703cf4b3fa74a0a9cd96c972a828d507ee81
JM <jmarmucki@tm1.edu.pl>
2024-03-13 11:15:25 +0100

    Move CV data to TS file

---
+++ src/data/cv.data.ts
@@ -0,0 +1,31 @@
+export const CV= {
+    name: 'Jan Marmucki',
+    bannerSentence_start: 'I build',
+    bannerSentences: [
+        'web apps',
+        'backend apps',
+        'Kubernetes clusters',
+        'cloud infrastructure',
+        'utility apps',
+        'web APIs'
+    ],
+
+    aboutMe: `
+I'm an ambitious student of programming class.
+I love learning new informatic skills and broading my knowledge in the areas I already know.
+Currently, my goal is to learn machine learning and deep learning.`,
+
+    technologiesIntroduction: `Technologies that I\'m using in my projects:`,
+    technologiesUsed: [
+        'programming languages (C, C++, C# Java, Python, Rust)',
+        'web development (HTML, CSS, JavaScript, TypeScript)',
+        'web frameworks and libraries (React, Angular, Svelte)',
+        'databases (MariaDB, Postgres, MongoDB, Neo4j)',
+        'Linux',
+        'Azure cloud',
+        'Docker',
+        'Kubernetes',
+        'Terraform'
+    ]
+
+}
