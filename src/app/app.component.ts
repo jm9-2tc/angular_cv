4166e72a085f99e0cc511a84ef21fab9af862c02
JM <jmarmucki@tm1.edu.pl>
2024-03-30 16:01:39 +0100

    Add theme and typography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { SiteAboutComponent } from "./components/site-about/site-about.component";
 
 @Component({
@@ -8,8 +12,9 @@ import { SiteAboutComponent } from "./components/site-about/site-about.component
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, MatTabsModule, SiteAboutComponent]
+    imports: [RouterOutlet, MatTabsModule, MatIconModule, MatButtonModule, SiteAboutComponent]
 })
 export class AppComponent {
   title = 'angular-cv';
+  useDarkMode = false;
 }
