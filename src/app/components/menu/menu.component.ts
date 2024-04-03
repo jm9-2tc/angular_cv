7f7cf2c2d7d0af17cdb8a952fc1b0820f40068c6
JM <jmarmucki@tm1.edu.pl>
2024-04-03 20:12:08 +0200

    Fix menu buttons style and add highlight for current section

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -8,8 +8,13 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
-  private useEnglish;
+export class MenuComponent implements AfterViewInit {
+
+  @ViewChild('selectedByDefault')
+  private btnSelectedByDefaultRef!: ElementRef;
+
+  private useEnglish: boolean;
+  private lastClickedBtn?: HTMLElement;
 
   constructor(private translate: TranslateService) {
     translate.addLangs(['pl', 'en']);
@@ -17,8 +22,24 @@ export class MenuComponent {
     this.useEnglish = true;
   }
 
+  ngAfterViewInit(): void {
+    this.selectSectionBtn(this.btnSelectedByDefaultRef.nativeElement);
+  }
+
   switchLanguage() {
     this.useEnglish = !this.useEnglish;
     this.translate.use(this.useEnglish ? 'en' : 'pl');
+  }
+
+  onSectionBtnClicked(event: Event) {
+    this.selectSectionBtn(event.target as HTMLElement);
+  }
+
+  selectSectionBtn(btn: HTMLElement) {
+    if (this.lastClickedBtn) {
+      this.lastClickedBtn.classList.remove('selected');
+    }
+    this.lastClickedBtn = btn;
+    this.lastClickedBtn.classList.add('selected');
   }
 }
